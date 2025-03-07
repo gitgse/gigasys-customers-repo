6018fccb18c9c373774b4ac1f4c854cba5bafb41
Gilles <gilles@gitlab>
2025-03-07 23:39:37 +0100

    first commit

---
+++ src/main/java/com/gigasys/customersrepo/CustomersrepoApplication.java
@@ -0,0 +1,13 @@
+package com.gigasys.customersrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gigasys/customersrepo/CustomersrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigasys.customersrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
