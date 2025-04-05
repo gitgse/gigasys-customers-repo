689821d0934db3b0696851b3fa93ffc326ae04e6
Gilles <git@example.com>
2025-04-05 23:46:31 +0200

    add tolowercase of strings when persisting/updating customer entity

--- src/main/java/com/gigasys/customerrepo/entities/Customer.java
+++ src/main/java/com/gigasys/customerrepo/entities/Customer.java
@@ -1,6 +1,20 @@
 package com.gigasys.customerrepo.entities;
 
+import static com.gigasys.customerrepo.common.Constants.ADDRESS_DETAILS_MAX_SIZE;
+import static com.gigasys.customerrepo.common.Constants.CITY_MAX_SIZE;
+import static com.gigasys.customerrepo.common.Constants.COUNTRY_SIZE;
+import static com.gigasys.customerrepo.common.Constants.CUSTOMER_FIRSTNAME_MAX_SIZE;
+import static com.gigasys.customerrepo.common.Constants.CUSTOMER_NAME_MAX_SIZE;
+import static com.gigasys.customerrepo.common.Constants.EMAIL_MAX_SIZE;
+import static com.gigasys.customerrepo.common.Constants.ENCRYPTED_PASSWORD_SIZE;
+import static com.gigasys.customerrepo.common.Constants.PHONE_MAX_SIZE;
+import static com.gigasys.customerrepo.common.Constants.POSTAL_CODE_MAX_SIZE;
+import static com.gigasys.customerrepo.common.Constants.REGION_MAX_SIZE;
+import static com.gigasys.customerrepo.common.Constants.STREET_MAX_SIZE;
+
+import java.lang.reflect.Field;
 import java.time.LocalDate;
+import java.util.Locale;
 
 import org.hibernate.annotations.Check;
 import org.hibernate.validator.constraints.Length;
@@ -12,12 +26,13 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import static com.gigasys.customerrepo.common.Constants.*;
 
 /**
  * Customer.java
@@ -78,4 +93,21 @@ public class Customer {
 	@Length(max = ENCRYPTED_PASSWORD_SIZE)
 	private String password;
 
+    @PrePersist
+    @PreUpdate
+    private void lowercaseStrings() {
+        for (Field field : this.getClass().getDeclaredFields()) {
+            // Si le champ est de type String, on le met en minuscules
+            if (!field.getName().equalsIgnoreCase("password") && field.getType().equals(String.class)) {
+            	try {
+            		String value = (String) field.get(this);
+            		if (value != null) {
+            			field.set(this, value.toLowerCase(Locale.ROOT));
+            		}
+            	} catch(Exception ignored) {
+            	}
+            }
+        }
+    }
+	
 }
